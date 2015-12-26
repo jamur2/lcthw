38b7f7dd1ccc46ee4f72965ef05035bc982fecfe
John Murphy <jackie@jamur2.net>
2015-12-26 10:15:11 -0500

    exercise 1

---
+++ ex1/ex1_experiment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("Hello world\n");
+    printf("My name is %s\n", "Jackie");
+    printf("\tEven more text without newline");
+    printf("This is the end\n");
+
+    return 0;
+}
